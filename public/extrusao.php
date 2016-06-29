21a99114b38ae564af919e05ffd1f6b1622dfb9c
robmachado <linux.rlm@gmail.com>
2016-06-29 16:08:51 -0300

    [UPD] plas

--- public/extrusao.php
+++ public/extrusao.php
@@ -17,7 +17,15 @@ $cod = filter_input(INPUT_GET, 'cod', FILTER_SANITIZE_STRING);
 //por principio uma OP é produzida por uma unica extrusora
 
 
-$script = "<script src=\"js/op.js\"></script>";
+$script = "<script src=\"js/op.js\"></script>"
+        . "<script>
+            function checkInput(ob) {
+                var invalidChars = /[^0-9]/gi
+                if (invalidChars.test(ob.value)) {
+                    ob.value = ob.value.replace(invalidChars,\"\");
+                }
+            }
+        </script>";
 $title = "Extrusao";
 
 $body = "
@@ -26,43 +34,75 @@ $body = "
         <center>
         <h2>Etiquetas Extrusão</h2>
         </center>
-    </div> 
-    <div class=\"row\">
-        <br>
-        <div class=\"col-md-4\">
-            <form role=\"form\" method=\"POST\" action=\"extrusao.php\">
-                <div class=\"input-group\">
-                    <input type=\"text\" class=\"form-control\" id=\"numop\" name=\"numop\" value=\"$numop\" placeholder=\"Entre com o numero da OP\">
-                    <span class=\"input-group-btn\">
-                        <button class=\"btn btn-primary\" type=\"submit\"><span class=\"glyphicon glyphicon-search\"></span> Busca </button>
-                    </span>
-                </div>                    
-                <div class=\"input-group\">
-                    <label for=\"peca\">Numero da Bobina</label>
-                    <input type=\"text\" class=\"form-control\" id=\"peca\" name=\"peca\" value=\"$peca\" placeholder=\"Numero da bobina\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"cod\" name=\"cod\" value=\"$cod\" placeholder=\"Codigo do produto\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"pBruto\" name=\"pBruto\" value=\"$pBruto\" placeholder=\"Entre com o peso Bruto\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"tara\" name=\"tara\" value=\"$tara\" placeholder=\"Entre com o peso da tara\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"pLiq\" name=\"pLiq\" value=\"$pLiq\" placeholder=\"Peso Liquido\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"nExt\" name=\"nExt\" value=\"$nExt\" placeholder=\"Numero da extrusora\">
-                </div>                        
-                <div class=\"input-group\">                        
-                    <input type=\"text\" class=\"form-control\" id=\"nOp\" name=\"nOp\" value=\"$nOp\" placeholder=\"Operador\">
-                </div>
-            </form>
-        </div>
-        <div class=\"col-md-1\"> </div>
     </div>
+    <br><br>
+    <form role=\"form\" method=\"POST\" action=\"extrusao.php\">
+    <div class=\"row\">
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"numop\" name=\"numop\" value=\"$numop\" placeholder=\"Entre com o numero da OP\" onkeyup=\"checkInput(this)\">
+                <span class=\"input-group-btn\">
+                    <button class=\"btn btn-primary\" type=\"button\" id=\"btnGetOP\" name=\"btnGetOP\"><span class=\"glyphicon glyphicon-search\"></span> Busca </button>
+                </span>
+            </div>
+        </div>    
+        <div class=\"col-md-4\">               
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"cod\" name=\"cod\" value=\"$cod\" placeholder=\"Codigo do produto\">
+            </div>                        
+        </div>
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"peca\" name=\"peca\" value=\"$peca\" placeholder=\"Numero da bobina\">
+            </div>                        
+        </div>    
+    </div>
+    <br>
+    <div class=\"row\">
+          <div class=\"col-md-4\">
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"desc\" name=\"desc\" value=\"$cod\" placeholder=\"Descrição do produto\">
+            </div>
+        </div>
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"nExt\" name=\"nExt\" value=\"$nExt\" placeholder=\"Numero da extrusora\">
+            </div>                        
+        </div>
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">                        
+                <input type=\"text\" class=\"form-control\" id=\"operador\" name=\"operador\" value=\"$nOp\" placeholder=\"Operador\">
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class=\"row\">
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">                        
+             <input type=\"text\" class=\"form-control\" id=\"pBruto\" name=\"pBruto\" value=\"$pBruto\" placeholder=\"Entre com o peso Bruto\">
+            </div>                 
+        </div>
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">                        
+                <input type=\"text\" class=\"form-control\" id=\"tara\" name=\"tara\" value=\"$tara\" placeholder=\"Entre com o peso da tara\">
+            </div>                        
+        </div>
+        <div class=\"col-md-4\">
+            <div class=\"input-group\">
+                <input type=\"text\" class=\"form-control\" id=\"pLiq\" name=\"pLiq\" value=\"$pLiq\" placeholder=\"Peso Liquido\">
+            </div>                        
+        </div>
+    </div>
+    <br>
+    <div class=\"row\">
+        <div class=\"col-md-6\">
+            <button type=\"button\" class=\"btn btn-success \" id=\"btnExtSave\" name=\"btnExtSave\"><span class=\"glyphicon glyphicon-floppy-disk\"></span>  Gravar e Imprimir</button>
+        </div>
+        <div class=\"col-md-6\">
+            <button type=\"button\" class=\"btn btn-info \" id=\"btnExtPrint\" name=\"btnExtPrint\"><span class=\"glyphicon glyphicon-print\"></span>   Apenas Imprimir</button>
+        </div>
+    </div>
+    </form>
     <div class=\"row\">    
         <div id = \"alert_placeholder\"></div>
     </div>    
