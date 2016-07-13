14af19a47b50a5c326181692687c61c2a7776d4d
robmachado <linux.rlm@gmail.com>
2016-07-13 17:31:26 -0300

    [UPD] 23

--- public/extrusao.php
+++ public/extrusao.php
@@ -8,9 +8,116 @@ require_once '../bootstrap.php';
 //por principio uma OP é produzida por uma unica extrusora
 
 
-$script = "";
+$script = "<script type=\"text/javascript\" language=\"javascript\">"
+    . "
+        qz.security.setCertificatePromise(function(resolve, reject) {
+            $.ajax(\"assets/signing/plastfoam-certificate.txt\").then(resolve, reject);
+        });
+
+        qz.security.setSignaturePromise(function(toSign) {
+            return function(resolve, reject) {
+                $.ajax(\"assets/signing/sign-message.php?request=\" + toSign).then(resolve, reject);
+            };
+        });
         
-$extras = "<script type = \"text/javascript\" language = \"javascript\">
+        qz.websocket.setClosedCallbacks(function(evt) {
+            updateState('Inactive', 'default');
+            console.log(evt);
+            if (evt.reason) {
+                displayMessage(\"<strong>Connection closed:</strong> \" + evt.reason, 'alert-warning');
+            }
+        });
+
+        qz.websocket.setErrorCallbacks(handleConnectionError);
+
+        function findDefaultPrinter(set) {
+            qz.printers.getDefault().then(function(data) {
+                if (set) { setPrinter(data); }
+            }).catch(displayError);
+        }
+        
+        function handleConnectionError(err) {
+            //updateState('Error', 'danger');
+            if (err.target != undefined) {
+                if (err.target.readyState >= 2) { //if CLOSING or CLOSED
+                    displayError(\"Connection to QZ Tray was closed\");
+                } else {
+                    displayError(\"A connection error occurred, check log for details\");
+                    console.error(err);
+                }
+            } else {
+                displayError(err);
+            }
+        }
+        
+        function displayError(err) {
+            console.error(err);
+            displayMessage(err, 'alert-danger');
+        }
+
+        function displayMessage(msg, css) {
+            if (css == undefined) { css = 'alert-info'; }
+            var timeout = setTimeout(function() { $('#' + timeout).alert('close'); }, 5000);
+            var alert = $(\"<div/>\").addClass('alert alert-dismissible fade in ' + css)
+                .css('max-height', '20em').css('overflow', 'auto')
+                .attr('id', timeout).attr('role', 'alert');
+            alert.html(\"<button type='button' class='close' data-dismiss='alert'>&times;</button>\" + msg);
+            $(\"#qz-alert\").append(alert);
+        }
+        
+        function launchQZ() {
+            if (!qz.websocket.isActive()) {
+                window.location.assign(\"qz:launch\");
+                //Retry 5 times, pausing 1 second between each attempt
+                startConnection({ retries: 5, delay: 1 });
+            }
+        }
+        
+        function startConnection(config) {
+            if (!qz.websocket.isActive()) {
+                //updateState('Waiting', 'default');
+                qz.websocket.connect(config).then(function() {
+                    //updateState('Active', 'success');
+                    //findVersion();
+                }).catch(handleConnectionError);
+            } else {
+                displayMessage('An active connection with QZ already exists.', 'alert-warning');
+            }
+        }
+        
+        function printBase64() {
+            var config = getUpdatedConfig();
+            var printData = [
+                {
+                    type: 'raw',
+                    format: 'base64',
+                    data: ''
+                }
+            ];
+            qz.print(config, printData).catch(displayError);
+        }
+        
+        function printEPL() {
+            var config = getUpdatedConfig();
+            var printData = [
+                '\nN\n',
+                'q609\n',
+                'Q203,26\n',
+                'B5,26,0,1A,3,7,152,B,\"1234\"\n',
+                'A310,26,0,3,1,1,N,\"SKU 00000 MFG 0000\"\n',
+                'A310,56,0,3,1,1,N,\"QZ PRINT APPLET\"\n',
+                'A310,86,0,3,1,1,N,\"TEST PRINT SUCCESSFUL\"\n',
+                'A310,116,0,3,1,1,N,\"FROM SAMPLE.HTML\"\n',
+                'A310,146,0,3,1,1,N,\"QZ.IO\"\n',
+                '\nP1,1\n'
+            ];
+            qz.print(config, printData).catch(displayError);
+        }
+    
+    "
+    . "</script>";
+        
+$extras = "<script type=\"text/javascript\" language=\"javascript\">
             $(document).ready(function() {
                 $('#numop').focus();
                 $('#numop').on('keydown', function(event) {
@@ -18,6 +125,11 @@ $extras = "<script type = \"text/javascript\" language = \"javascript\">
                         getValues();
                     }    
                 });
+                
+                //QZ Print Set Default Printer
+                startConnection();
+                //findDefaultPrinter(true);
+                
                 $('#btnGetOP').click(function () {
                     getValues();
                 });
@@ -53,8 +165,9 @@ $extras = "<script type = \"text/javascript\" language = \"javascript\">
                     }    
                 });
                 $('#btnExtSave').click(function (event) {
-                    event.preventDefault();
-                    clearValues();
+                    //event.preventDefault();
+                    //clearValues();
+                    findDefaultPrinter(true);
                 });
                 $('#btnExtPrint').click(function (event) {
                     clearValues();
@@ -105,6 +218,7 @@ $body = "
         <h2>Etiquetas Extrusão</h2>
         </center>
     </div>
+    <div id=\"qz-alert\" style=\"position: fixed; width: 60%; margin: 0 4% 0 36%; z-index: 900;\"></div>
     <br><br>
     <form role=\"form\" method=\"POST\" action=\"extrusao.php\">
     <div class=\"row\">
